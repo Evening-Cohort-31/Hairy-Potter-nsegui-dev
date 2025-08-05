15327f7193bee88ea839c17741ebf5a0d2671234
Nick Segui <nicksegui5@gmail.com>
2025-08-05 18:50:18 -0500

    added PotteryWheel.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,19 @@
 // Imports go first
-
-
+import { makePottery } from "./PotteryWheel.js";
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
-
-
+let mug = makePottery("mug", 2, 5);
+let platter = makePottery("platter", 5, 1);
+let goblet = makePottery("goblet", 3, 7);
+let vase = makePottery("vase", 10, 15);
+let statue = makePottery("statue", 15, 14);
 // Fire each piece of pottery in the kiln
-
 
 // Determine which ones should be sold, and their price
 
-
 // Invoke the component function that renders the HTML list
-
+console.log(mug);
+console.log(platter);
+console.log(goblet);
+console.log(vase);
+console.log(statue);
