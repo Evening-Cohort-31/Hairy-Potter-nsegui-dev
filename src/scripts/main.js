a297684938eab2768dbf8cfd56a0eff22dd1f299
Nick Segui <nicksegui5@gmail.com>
2025-08-11 19:57:07 -0500

    added toSell functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js";
 import { firePottery } from "./Kiln.js";
+import { toSellOrNotToSell, usePottery } from "./PotteryCatalog.js";
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("mug", 2, 5);
 let platter = makePottery("platter", 5, 1);
@@ -14,10 +15,17 @@ let firedGoblet = firePottery(goblet, 1500);
 let firedVase = firePottery(vase, 2300);
 let firedStatue = firePottery(statue, 2400);
 // Determine which ones should be sold, and their price
+toSellOrNotToSell(firedMug);
+toSellOrNotToSell(firedPlatter);
+toSellOrNotToSell(firedGoblet);
+toSellOrNotToSell(firedVase);
+toSellOrNotToSell(firedStatue);
 
+let itemsForSale = usePottery();
 // Invoke the component function that renders the HTML list
-console.log(firedMug);
-console.log(firedPlatter);
-console.log(firedGoblet);
-console.log(firedVase);
-console.log(firedStatue);
+//console.log(firedMug);
+//console.log(firedPlatter);
+//console.log(firedGoblet);
+//console.log(firedVase);
+//console.log(firedStatue);
+console.table(itemsForSale);
